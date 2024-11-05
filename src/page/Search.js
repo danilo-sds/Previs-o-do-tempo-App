bfd498ff80921afaecac5804e78024a1ebd624aa
danilo-sds <danilosouza302@gmail.com>
2024-11-05 00:07:55 -0300

    Requisição Automatizada na API

--- src/page/Search.js
+++ src/page/Search.js
@@ -3,19 +3,31 @@ import "../App.css";
 function Search() {
   function searchInput() {
     let currentValue = document.querySelector("input[name=search").value;
-    alert(currentValue);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const { main, name, sys, weather } = data;
+        if (sys !== undefined) console.log(sys);
+        if (weather !== undefined) console.log(weather[0]["description"]);
+      });
   }
 
   return (
     <div className="search">
       <h2>Digite a cidade que você quer saber a previsão</h2>
-      <input
-        onKeyUp={searchInput}
-        type="text"
-        name="search"
-        id="search"
-        placeholder="Digite o nome da cidade..."
-      />
+      <form onSubmit={(e) => searchInput(e)}>
+        <input
+          onKeyUp={searchInput}
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Digite o nome da cidade..."
+        />
+        <input type="submit" value="Pesquise por cidade!" />
+      </form>
     </div>
   );
 }
